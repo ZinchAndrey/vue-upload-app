b7867a89ad9f6eaac29fdf227ee9291130c35faf
ZinchAndrey <zinchandrey@gmail.com>
2024-04-28 01:09:36 +0300

    add project description at readme.md

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,23 @@ import svgLoader from 'vite-svg-loader'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-    svgLoader({
-      defaultImport: 'url' // or 'raw'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return defineConfig({
+    base: isProduction ? '/'  : '/vue-upload-app/',
+  
+    plugins: [
+      vue(),
+      VueDevTools(),
+      svgLoader({
+        defaultImport: 'url' // or 'raw'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  })
+}
